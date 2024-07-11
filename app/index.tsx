c3595874b695e8345d58dd6f5ff5a898751547de
Sebastian <seby.danyel@gmail.com>
2024-07-11 03:30:42 +0300

    style: added react native reusuables and landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { Redirect, router } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { images } from '@/constants';
-import CustomButton from '@/components/CustomButton';
-import { useGlobalContext } from '@/context/GlobalProvider';
+import { StatusBar } from "expo-status-bar";
+import { View, Image, Dimensions } from "react-native";
+import { Redirect, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+const logoBlue = require("@/assets/images/fo-blue.png");
 
 export default function App() {
     const { isLoading, isLoggedIn } = useGlobalContext();
+    let deviceH = Dimensions.get("screen").height;
+    let windowH = Dimensions.get("window").height;
+    let bottomNavBarH = deviceH - windowH;
 
     if (!isLoading && isLoggedIn) return <Redirect href="/home" />;
 
     return (
-        <SafeAreaView className="bg-primary h-full">
-            <View className="w-full justify-center items-center min-h-[85vh]">
-                <CustomButton
-                    title="Continue with Google"
-                    handlePress={() => {
-                        router.push('/sign-in');
-                    }}
-                    containerStyles="w-full mt-7"
-                />
-                <CustomButton
-                    title="Sign Up"
-                    handlePress={() => {
-                        router.push('/sign-up');
-                    }}
-                    containerStyles="w-full mt-7"
-                />
-                <CustomButton
-                    title="Sign In"
-                    handlePress={() => {
-                        router.push('/sign-in');
-                    }}
-                    containerStyles="w-full mt-7"
-                />
+        <SafeAreaView className="flex-1 bg-background">
+            <View className="flex h-full flex-col items-center justify-end overflow-auto p-4">
+                <View className="relative -top-10 left-12 h-full w-[125vw] rotate-6 bg-secondary">
+                    <Image
+                        source={logoBlue}
+                        className="absolute -left-16 bottom-0 h-[525px] w-[525px] rotate-[-6deg]"
+                    />
+                </View>
+                <View className="w-full items-center">
+                    <Text className="font-psemibold text-4xl text-primary">
+                        Spend Smarter
+                    </Text>
+                    <Text className="mb-10 font-psemibold text-4xl text-primary">
+                        Save More
+                    </Text>
+                    <Button
+                        onPress={() => router.push("/sign-up")}
+                        className="mb-4 w-full rounded-full"
+                        size={"lg"}
+                    >
+                        <Text className="text-5xl">Google</Text>
+                    </Button>
+                    <Button
+                        onPress={() => router.push("/sign-up")}
+                        className="mb-4 w-full rounded-full"
+                        size={"lg"}
+                    >
+                        <Text className="text-5xl">Sign Up</Text>
+                    </Button>
+                    <Button
+                        onPress={() => router.push("/sign-in")}
+                        variant={"secondary"}
+                        className="w-full rounded-full"
+                        size={"lg"}
+                    >
+                        <Text className="text-5xl">Sign In</Text>
+                    </Button>
+                </View>
             </View>
             <StatusBar backgroundColor="#FFF" style="dark" />
         </SafeAreaView>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,63 @@
+const { hairlineWidth } = require("nativewind/theme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    darkMode: "class",
     content: [
-        './app/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}'
+        "./app/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}"
     ],
+    presets: [require("nativewind/preset")],
     theme: {
         extend: {
             colors: {
-                primary: '#FFF',
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))"
+                },
                 secondary: {
-                    DEFAULT: '#0088ff',
-                    100: '#80c5ff',
-                    200: '#339aff'
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))"
                 },
-                black: {
-                    DEFAULT: '#000',
-                    100: '#1E1E2D',
-                    200: '#232533'
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))"
                 },
-                gray: {
-                    100: '#CDCDE0'
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))"
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))"
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))"
+                },
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))"
                 }
             },
             fontFamily: {
-                pthin: ['Poppins-Thin', 'sans-serif'],
-                pextralight: ['Poppins-ExtraLight', 'sans-serif'],
-                plight: ['Poppins-Light', 'sans-serif'],
-                pregular: ['Poppins-Regular', 'sans-serif'],
-                pmedium: ['Poppins-Medium', 'sans-serif'],
-                psemibold: ['Poppins-SemiBold', 'sans-serif'],
-                pbold: ['Poppins-Bold', 'sans-serif'],
-                pextrabold: ['Poppins-ExtraBold', 'sans-serif'],
-                pblack: ['Poppins-Black', 'sans-serif']
+                pthin: ["Poppins-Thin", "sans-serif"],
+                pextralight: ["Poppins-ExtraLight", "sans-serif"],
+                plight: ["Poppins-Light", "sans-serif"],
+                pregular: ["Poppins-Regular", "sans-serif"],
+                pmedium: ["Poppins-Medium", "sans-serif"],
+                psemibold: ["Poppins-SemiBold", "sans-serif"],
+                pbold: ["Poppins-Bold", "sans-serif"],
+                pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+                pblack: ["Poppins-Black", "sans-serif"]
+            },
+            borderWidth: {
+                hairline: hairlineWidth()
             }
         }
     },
